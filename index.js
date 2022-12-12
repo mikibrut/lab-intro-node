2a5a0a47bc59b4a1bd4a7438ce29f2a0ad1baa2c
Miki Brut <martiejarque@gmail.com>
2022-12-13 00:12:19 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error('OutOfBounds');
+    };
+    return this.items[pos];
+    }
+    
 
-  max() {}
+  max() {
+    if(!this.items.length){
+      throw new Error('EmptySortedList');;
+    };
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length){
+      throw new Error('EmptySortedList');;
+    };
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    let sumatory = this.items.reduce((a, b) => a + b, 0);
+    return sumatory;
+    if (sumatory <= 0){
+      return '0';
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(!this.items.length){
+      throw new Error('EmptySortedList');
+    } else {
+    let average = this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    return average; 
+   };
+        
+  }
 }
 
 module.exports = SortedList;
